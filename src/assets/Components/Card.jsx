a93ffd1e9e45ccf11b2a07a53dfc30e9acb1c0af
LucasVidalle <lucas.vidalle@gm2dev.com>
2023-09-19 13:01:57 -0300

    feat: progress with css

--- src/assets/Components/Card.jsx
+++ src/assets/Components/Card.jsx
@@ -1,22 +1,20 @@
-import "./Card.css"
+import "./Card.css";
 
-function Card ({title, price, description, image}){
+function Card({ title, price, description, image }) {
     return (
         <div className="structure-card">
-            <div className="content-card">
-                    <img className="img-card" src={image}/>
-                    <div className="info-product">
-                        <h1 className="title-product">{title}</h1>
-                        <h2 className="description-product">{description}</h2>
-                        <h3 className="price-product">{price}</h3>
-                    </div>
-                    <div className="footer-card">
-                        <button className="recommend"></button>
-                        <button className="buy"></button>
-                    </div>
+            <img className="img-card" src={image} />
+            <div className="info-product">
+                <h1 className="title-product">{title}</h1>
+                <h2 className="description-product">{description}</h2>
+                <h3 className="price-product">${price}</h3>
+            </div>
+            <div className="footer-card">
+                <button className="recommend">RECOMMEND</button>
+                <button className="buy">BUY</button>
             </div>
         </div>
-    )
+    );
 }
 
-export { Card }
+export { Card };
--- src/assets/Components/Nav.jsx
+++ src/assets/Components/Nav.jsx
@@ -1,9 +1,12 @@
-import "./Nav.css"
+import "./Nav.css";
 
-function Nav (){
-    <div className="nav-bar">
-        <p className="nav-bar-title">The best fucking ecommerce in the world</p>
-    </div>
+function Nav() {
+    return (
+        <div className="nav-bar">
+            <p className="nav-bar-title">The best fucking ecommerce in the world</p>
+        </div>
+    )
+
 }
 
-export { Nav }
+export { Nav };
